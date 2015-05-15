260c3dcaa68e011cb0020849bccd4aca4fb62684
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-05-15 10:25:23 -0400

    Improve forms' views using Bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title="")
-    base_title = "Sample App 1"
+    base_title = "MNISHIGUCHI"
 
     if page_title.empty?
       base_title
