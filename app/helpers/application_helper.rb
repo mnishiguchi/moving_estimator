2823a4175294b52355a343e72dd5742208bf35a2
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-05-16 18:01:14 -0400

    Add gravatar_for method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,13 @@ module ApplicationHelper
       "#{page_title} | #{base_title}"
     end
   end
+
+  # return the Gravatar image tag for the given user
+  def gravatar_for(email, options = { size: 80 })
+    # standardize on all lower-case addresses
+    gravatar_id = Digest::MD5::hexdigest(email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: email, class: "gravatar")
+  end
 end
