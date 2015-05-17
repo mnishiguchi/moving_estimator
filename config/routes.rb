1b79f9c6309e09f8aab54435ebd8c7863e861140
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-05-16 20:39:37 -0400

    Add routes for contact; Improve contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :contacts, only: [:new, :create]
+  # resources :contacts, only: [:new, :create]
+  get  'contact' => 'contacts#new'
+  post 'contact' => 'contacts#create'
 
   devise_for :users
   root 'static_pages#home'
