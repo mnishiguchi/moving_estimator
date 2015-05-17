91ce48e5c00f602f9f202530a944cc379baa6911
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-05-17 17:46:58 -0400

    Add friendly-login upon succesful confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get  'contact' => 'contacts#new'
   post 'contact' => 'contacts#create'
 
-  devise_for :users
+  devise_for :users, controllers: { confirmations: 'confirmations' }
   root 'static_pages#home'
 end
