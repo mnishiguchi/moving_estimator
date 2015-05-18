313ebefc87e073275a329d2452fea1a551dc4986
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-05-18 14:22:25 -0400

    Set up RSpec; Add spec for contact-mailer

--- spec/mailers/contact_mailer_spec.rb
+++ spec/mailers/contact_mailer_spec.rb
@@ -1,17 +1,25 @@
 require "rails_helper"
 
 RSpec.describe ContactMailer, :type => :mailer do
-  describe "message_from_user" do
-    let(:mail) { ContactMailer.message_from_user }
 
-    it "renders the headers" do
-      expect(mail.subject).to eq("Message from user")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+  describe "Contact email from user" do
+
+    let(:mail) do
+      @contact = Contact.new(first_name: "Example",
+                             last_name:  "User",
+                             email:      "user@example.com",
+                             message:    "Lorem ipsum dolor sit amet.")
+      ContactMailer.message_from_user(@contact)
     end
 
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+    it "renders the email header" do
+      expect(mail.subject).to eq('A message from "Example User" <user@example.com>')
+      expect(mail.to).to eq(["masatoshi.nishiguchi@udc.edu"])
+      expect(mail.from).to eq([@contact.email])
+    end
+
+    it "renders user's message in the body" do
+      expect(mail.body.encoded).to match(@contact.message)
     end
   end
 
---
+++ spec/models/contact_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Contact do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
