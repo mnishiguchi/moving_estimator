72b96f272a036e1b9e1bc960c2459e91838d304e
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-05-18 15:52:05 -0400

    Improve contact-spec

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -16,31 +16,39 @@ describe Contact do
   it "has non-blank first name" do
     contact.first_name = "  "
     expect(contact).not_to be_valid
-    expect(contact.errors[:first_name]).to include("can't be blank")
+    expect(contact.errors[:first_name]).to_not be_nil
   end
 
   it "has non-blank last name" do
     contact.last_name = "  "
     expect(contact).not_to be_valid
-    expect(contact.errors[:last_name]).to include("can't be blank")
+    expect(contact.errors[:last_name]).to_not be_nil
   end
 
   it "has non-blank email" do
     contact.email = "  "
     expect(contact).not_to be_valid
-    expect(contact.errors[:email]).to include("can't be blank")
+    expect(contact.errors[:email]).to_not be_nil
   end
 
   it "has email with at most 255 characters" do
-    contact.email = "a" * 255
+    contact.email = "a" * 256
     expect(contact).not_to be_valid
+    expect(contact.errors[:email]).to_not be_nil
   end
 
   it "has non-blank message" do
     contact.message = "  "
     expect(contact).not_to be_valid
-    expect(contact.errors[:message]).to include("can't be blank")
+    expect(contact.errors[:message]).to_not be_nil
   end
 
+  it "has message with at most 255 characters" do
+    contact.message = "a" * 256
+    expect(contact).not_to be_valid
+    expect(contact.errors[:message]).to_not be_nil
+  end
+
+  # TODO
   it "rejects invalid email format"
 end
