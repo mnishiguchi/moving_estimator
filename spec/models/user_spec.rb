d171b841929d4ea2473037ae93e25a5d3359ca6b
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-05-19 13:30:49 -0400

    Add guard-rspec for automated testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,7 +22,7 @@
 
 require 'rails_helper'
 
-# describe User do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+describe User do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
 
