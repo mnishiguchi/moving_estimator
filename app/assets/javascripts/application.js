136777cd0b2a5ae9fd6ad6fa641ee0a17b1286aa
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-05-25 11:08:34 -0400

    See if this will get metisMenu working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,10 @@
 //= require_tree .
 //
 // Vendor Files
-//= require metisMenu.js
-//= require sb_admin_2.js
-//= require dataTables.responsive.js
+//= require metisMenu
+//= require sb_admin_2
+//= require dataTables.responsive
+
+//= depend_on_asset "metisMenu.js"
+//= depend_on_asset "sb_admin_2.js"
+//= depend_on_asset "dataTables.responsive.js"
