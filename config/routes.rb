bd5496d5f197ab6d1f5875e64de20a3b6e4a0804
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-05-28 11:09:57 -0400

    Fix script for navbar header

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { confirmations: 'confirmations' }
   resources  :users, only: [:index, :show, :destroy]
   get  'dashboard' => 'users#show'
+
   get  'about'     => 'static_pages#about'
   get  'contact'   => 'contacts#new'
   post 'contact'   => 'contacts#create'
