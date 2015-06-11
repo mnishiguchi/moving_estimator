3ad8c3af092bc2cdfe6ebd7eb30322f068ab979a
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-06-11 11:37:26 -0400

    Add prototype template for todolist

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,11 +3,12 @@ class TodosController < ApplicationController
   before_action :authenticate_user! # all actions
 
   def index
-    @todos = Todo.select("id, content").where(user: current_user).to_json
+    @todos = current_user.todos
+    # @todos = Todo.select("id, content").where(user: current_user).to_json
   end
 
   def create
-    @todo = current_user.todos.build(todo_params)
+    @todo = current_user.todos.create!(todo_params)
   end
 
   def update
