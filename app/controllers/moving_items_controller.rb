48d0c7e286e62cdcb8d10e71457cc634238c637d
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-06-30 10:55:07 -0400

    Refactor add item fields

--- app/controllers/moving_items_controller.rb
+++ app/controllers/moving_items_controller.rb
@@ -2,12 +2,14 @@ class MovingItemsController < ApplicationController
 
   include MovingsHelper
 
+  before_action :authenticate_user! # all actions
+  before_action :ensure_moving!     # all actions
+
   # Note: A list of items is displayed in Movings#show page instead of MovingItems#index.
 
   # Shows a form to create a new item.
   def new
     @moving_item = MovingItem.new
-    @moving_id = current_moving
   end
 
   def create
@@ -33,7 +35,7 @@ class MovingItemsController < ApplicationController
     @moving_item = MovingItem.find(params[:id])
     @moving_item.update_columns(moving_item_params)
     flash[:success] = "Item updated"
-    redirect_to moving_url(current_moving)
+    redirect_to moving_url(@moving)
   end
 
   # Delete the moving item record.
@@ -49,5 +51,10 @@ class MovingItemsController < ApplicationController
     def moving_item_params
       params.require(:moving_item).permit(:name, :volume, :quantity,
                                           :description, :room, :category)
+    end
+
+    # Ensures that current moving actually exists in database.
+    def ensure_moving!
+      @moving = Moving.find(current_moving)
     end
 end
--- app/controllers/movings_controller.rb
+++ app/controllers/movings_controller.rb
@@ -14,7 +14,8 @@ class MovingsController < ApplicationController
     id = params[:id] || current_moving
     @moving = Moving.find(id)
     @moving_items = @moving.moving_items
-    remember_moving @moving  # Remember moving id for later use.
+    @moving_item = MovingItem.new  # For new form
+    remember_moving @moving          # Remember moving id for later use.
   end
 
   # Shows a form to create a new moving.
@@ -51,6 +52,7 @@ class MovingsController < ApplicationController
   # Delete the moving record.
   def destroy
     Moving.find(params[:id]).destroy
+    forget_moving
     flash[:success] = "Moving deleted"
     redirect_to movings_url
   end
