8483ea509ed7385f1e9becc548e34bf16c314a88
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-07-01 22:55:23 -0400

    Make add form inline

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,14 @@ module ApplicationHelper
       "#{page_title} | #{base_title}"
     end
   end
+
+  # collection: an array of objects
+  # prop:       attribute
+  def list_tag(collection, prop)
+    content_tag(:ul) do
+      collection.each do |elem|
+        concat content_tag(:li, elem.send(prop))
+      end
+    end
+  end
 end
