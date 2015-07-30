cb9a8f93fe05910ead4d46b1327a0530419eb329
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-07-30 14:30:29 -0400

    Refactor OmniauthCallbacksController

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -4,8 +4,8 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     # raise request.env["omniauth.auth"].to_yaml
     user = User.from_omniauth(request.env["omniauth.auth"])
     if user.persisted?
-      flash[:notice] = "Logged in!"
-      sign_in_and_redirect user
+      sign_in_and_redirect user, event: :authentication
+      set_flash_message(:notice, :success, kind: __callee__) if is_navigational_format?
     else
       session["devise.user_attributes"] = user.attributes
       redirect_to new_user_registration_url
--- app/models/user.rb
+++ app/models/user.rb
@@ -65,6 +65,8 @@ class User < ActiveRecord::Base
     current_moving = nil
   end
 
+  # ==> OmniAuth
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
