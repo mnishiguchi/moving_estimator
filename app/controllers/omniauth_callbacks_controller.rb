bbd89ca9549da9aec9d29bbd54a0e9700c1f8cb8
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-08-02 00:05:11 -0400

    Clean up after merging

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -4,11 +4,11 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     # raise request.env["omniauth.auth"].to_yaml
     user = User.from_omniauth(request.env["omniauth.auth"])
     if user.persisted?  # Ensure that this user is saved to database.
-      sign_in_and_redirect user, event: :authentication
-      set_flash_message(:notice, :success, kind: __callee__) if is_navigational_format?
+      sign_in_and_redirect user
+      set_flash_message(:notice, :success, kind: __callee__.to_s.capitalize) if is_navigational_format?
     else
       session["devise.user_attributes"] = user.attributes
-      redirect_to new_user_registration_url(omniauth_fail: "omniauth_fail")
+      redirect_to new_user_registration_url(from_omniauth_callback: "1")
     end
   end
 
