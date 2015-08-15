24f810c10d8a20da3d51f315304fcb28e3d12ef8
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-08-15 15:13:13 -0400

    Add dashboard_url to routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   resources  :users, only: [:index, :destroy]
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], as: :finish_signup
 
+  get  'dashboard' => 'movings#index'
   get  'contact'   => 'contacts#new'
   post 'contact'   => 'contacts#create'
 
